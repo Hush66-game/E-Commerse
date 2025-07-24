d375afa721311cbff10131bacb8c90e7bedabf50
Hush66-game <villacarlosadrian4@gmail.com>
2025-07-24 18:25:06 +0800

    added login page

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Route, useNavigate, Routes, Link } from "react-router-dom";
 import axios from "axios";
+import {
+  Box,
+  Input,
+  InputGroup,
+  Button,
+  HStack,
+  VStack,
+  Field,
+  defineStyle,
+  Flex,
+} from "@chakra-ui/react";
+import { LuUser, LuLock } from "react-icons/lu";
+import Register from "./register";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -14,24 +27,77 @@ const LoginPage = () => {
         email,
         password,
       });
-      localStorage.setItem("toke", res.data.token);
+      localStorage.setItem("token", res.data.token);
       navigate("/");
     } catch {
       alert("Login failed");
     }
   };
+
+  <Routes>
+    <Route path="/register" element={<Register />} />
+  </Routes>;
   return (
-    <div>
-      <h2>Login</h2>
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
-    </div>
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+      bg="gray.100"
+    >
+      <Box
+        bg="gray.100"
+        p={8}
+        borderRadius="lg"
+        boxShadow="md"
+        width="100%"
+        maxW="sm"
+      >
+        <Field.Root>
+          <Box position="relative" mb={4} width="100%">
+            <Input className="peer" placeholder="" />
+            <Field.Label css={floatingStyles}>Email</Field.Label>
+          </Box>
+
+          <Box position="relative" mb={6} width="100%">
+            <Input type="password" className="peer" placeholder="" />
+            <Field.Label css={floatingStyles}>Password</Field.Label>
+          </Box>
+
+          <Button colorScheme="teal" width="100%">
+            Log In
+          </Button>
+          <Button as={Link} to="/register" colorScheme="teal" width="100%">
+            Register
+          </Button>
+        </Field.Root>
+      </Box>
+    </Box>
   );
 };
+
+const floatingStyles = defineStyle({
+  pos: "absolute",
+  bg: "none",
+  px: ".5",
+  insetStart: "2",
+  top: "0.5rem",
+  left: "0.75rem",
+  fontSize: "0.875rem",
+  fontWeight: "normal",
+  pointerEvents: "none",
+  color: "black",
+  transition: "all 0.2s ease",
+  _peerPlaceholderShown: {
+    color: "fg.muted",
+    top: "2.5",
+    insetStart: "3",
+  },
+  _peerFocusVisible: {
+    color: "black",
+    top: "-3",
+    insetStart: "2",
+  },
+});
 
 export default LoginPage;
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+        <form action="">
+            <input type="username" placeholder='Username' />
+            <input type='password' placeholder='Password' />
+            <input type='confirmpassword' placeholder='confirmpassword'/>
+        </form>
+    </div>
+  )
+}
+
+export default Register
